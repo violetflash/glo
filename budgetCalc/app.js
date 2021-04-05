0e491a3b6d8e323f6f3f2d0e9fdd1b6fe5b5130a
Alex Grobovsky <ktotaam@mail.ru>
2021-04-05 10:51:16 +0500

    added lesson_03 regular

--- budgetCalc/app.js
+++ budgetCalc/app.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let money = 7500;
 let income = 'попрошайничество';
 let addExpenses = 'алкоголь,еда,оброк хозяину';
@@ -18,3 +20,37 @@ console.log(addExpenses.toLowerCase().split(','));
 
 let budgetDay = money / 30;
 console.log(budgetDay);
+
+
+money = +prompt('Ваш месячный доход?');
+addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+deposit = confirm('Есть ли у вас депозит в банке?');
+
+
+const expenses1 = prompt('Введите обязательную статью расходов:');
+let amount1 = +prompt('Во сколько это обойдется?');
+amount1 = isNaN(amount1) ? 0 : amount1;
+
+
+const expenses2 = prompt('Введите обязательную статью расходов:');
+let amount2 = +prompt('Во сколько это обойдется?');
+amount2 = isNaN(amount2) ? 0 : amount2;
+
+
+const budgetMonth = money - amount1 - amount2;
+console.log(`Бюджет на месяц: ${budgetMonth}`);
+
+console.log(`Цель (${mission} рублей) будет достигнута за ${Math.round(mission / budgetMonth)} месяца(-ев)`);
+
+budgetDay = budgetMonth / 30;
+console.log(`Дневной бюджет, руб. : ${Math.floor(budgetDay)}`);
+
+if (budgetDay >= 1200) {
+  console.log('У Вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay < 1200) {
+  console.log('У Вас средний уровень дохода');
+} else if (budgetDay >= 0 && budgetDay < 600) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else {
+  console.log('Что-то пошло не так');
+}

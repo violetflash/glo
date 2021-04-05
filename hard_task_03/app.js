0cd4964460f8580efefd7569f0d8ecf3e9db6f0a
Alex Grobovsky <ktotaam@mail.ru>
2021-04-05 11:48:10 +0500

    added hard task 03

--- hard_task_03/app.js
+++ hard_task_03/app.js
@@ -5,7 +5,49 @@
 через if,
 через switch-case
 через многомерный массив без ифов и switch.
+ */
+
+const en = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+const ru = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье',];
+
+const lang = confirm(" OК - 'ru'\n Отмена - 'en' ") ? 'ru' : 'en';
+
+if (lang === 'ru') {
+  console.log(ru.toString());
+} else if (lang === 'en') {
+  console.log(en.toString());
+}
+
+console.log('=========== SWITCH ============');
+
+switch (lang) {
+  case 'ru': {
+    console.log(ru.toString());
+    break;
+  }
+  case 'en': {
+    console.log(en.toString());
+    break;
+  }
+}
+
+console.log('=========== ARRAY ================');
+const days = [en, ru];
+
+lang === 'ru' ? console.log(days[1].toString()) : console.log(days[0].toString());
+
+
+
+console.log('========= PART 2 ==================')
+/*
 У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”,
 если значение “Максим” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”
 	Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
  */
+const namePerson = 'Максимка';
+
+namePerson === 'Артем' ? console.log('директор') :
+  namePerson === 'Максим' ? console.log('преподаватель') :
+    console.log('студент');
+
+

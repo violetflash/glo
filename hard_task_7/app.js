3828ddc0b6f1e0e6747a99ce5c2f049ebb2cf188
Alex Grobovsky <ktotaam@mail.ru>
2021-04-09 12:36:04 +0500

    added browser output

--- hard_task_7/app.js
+++ hard_task_7/app.js
@@ -9,6 +9,8 @@
 
 const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
 
+
+//В консоль
 week.forEach((day, idx) => {
   if (idx === 0 || idx === 6) {
     console.log(`%c${day}`, 'font-style:italic');
@@ -17,4 +19,16 @@ week.forEach((day, idx) => {
   } else {
     console.log(day);
   }
+});
+
+//в браузер
+week.forEach((day, idx) => {
+  const li = document.createElement('li');
+  if (idx === 0 || idx === 6) {
+    li.style.fontStyle = 'italic';
+  } else if (idx === new Date().getDay()) {
+    li.style.fontWeight = 700;
+  }
+  li.innerText = day;
+  document.querySelector('.days').append(li);
 });

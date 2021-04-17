799afd6ef54f94a111b6be8aee4d4815926c7826
Alex Grobovsky <ktotaam@mail.ru>
2021-04-17 14:35:52 +0500

    feat: add innerText if can't move further

--- hardTask_14/app.js
+++ hardTask_14/app.js
@@ -32,64 +32,67 @@ DomElement.prototype.createElement = function() {
 
 DomElement.prototype.moveUp = function(amount) {
   let element = document.querySelector(`${this.selector}`);
+  element.innerText = '';
   let topPosition = parseInt(getComputedStyle(element).top);
   if (element.getBoundingClientRect().y > amount) {
     element.style.top = topPosition - amount + 'px';
   } else {
-    console.log('There is no way UP!');
+    element.innerText = 'no way UP!';
   }
 };
 
 DomElement.prototype.moveDown = function(amount) {
   let element = document.querySelector(`${this.selector}`);
+  element.innerText = '';
   let topPosition = parseInt(getComputedStyle(element).top);
   if (element.getBoundingClientRect().bottom + amount < window.innerHeight) {
     element.style.top = topPosition + amount + 'px';
   } else {
-    console.log('There is no way DOWN!');
+    element.innerText = 'no way DOWN!';
   }
 
 };
 
 DomElement.prototype.moveLeft = function(amount) {
   let element = document.querySelector(`${this.selector}`);
+  element.innerText = '';
   let leftPosition = parseInt(getComputedStyle(element).left);
   if (element.getBoundingClientRect().x > amount) {
     element.style.left = leftPosition - amount + 'px';
   } else {
-    console.log('There is no way LEFT!');
+    element.innerText = 'no way LEFT!';
   }
 
 };
 
 DomElement.prototype.moveRight = function(amount) {
   let element = document.querySelector(`${this.selector}`);
+  element.innerText = '';
   let leftPosition = parseInt(getComputedStyle(element).left);
   if (element.getBoundingClientRect().right + amount < window.innerWidth) {
     element.style.left = leftPosition + amount + 'px';
   } else {
-    console.log('There is no way DOWN!');
+    element.innerText = 'no way RIGHT!';
   }
 };
-
 
 let elem1 = new DomElement('.square', 100, 100, 'lightblue', 16, 'absolute');
 
 
 window.addEventListener('DOMContentLoaded', function(e) {
   elem1.createElement();
-  window.addEventListener('keydown', function(e) {
-    if (e.key === 'ArrowUp') {
-      elem1.moveUp(10);
-    }
-    if (e.key === 'ArrowDown') {
-      elem1.moveDown(10);
-    }
-    if (e.key === 'ArrowLeft') {
-      elem1.moveLeft(10);
-    }
-    if (e.key === 'ArrowRight') {
-      elem1.moveRight(10);
-    }
-  });
+});
+window.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowUp') {
+    elem1.moveUp(10);
+  }
+  if (e.key === 'ArrowDown') {
+    elem1.moveDown(10);
+  }
+  if (e.key === 'ArrowLeft') {
+    elem1.moveLeft(10);
+  }
+  if (e.key === 'ArrowRight') {
+    elem1.moveRight(10);
+  }
 });

f5a3deb628d3719c117c5381dc5309a80d8261a0
Alex Grobovsky <ktotaam@mail.ru>
2021-04-21 21:58:11 +0500

    add hard2 - animation

--- hard2 - animation/app.js
+++ hard2 - animation/app.js
@@ -1,62 +1,77 @@
 'use strict';
 
-const input = document.querySelector('input');
-const start = document.querySelector('#start');
-const pause = document.querySelector('#pause');
-const stop = document.querySelector('#stop');
-let startedState = 0;
-let startValue = 0;
+const timer = {
+  input: document.querySelector('input'),
+  start: document.querySelector('#start'),
+  pause: document.querySelector('#pause'),
+  stop: document.querySelector('#stop'),
+  started: 0,
 
-input.addEventListener('input', function(e) {
-  this.value = this.value.replace(/[^\d]/g, '');
-});
 
-const tick = function(value, amount) {
-  return value - amount;
-};
+  eventsListeners() {
+    this.input.addEventListener('input', function(e) {
+      this.value = this.value.replace(/[^\d]/g, '');
+      timer.startValue = this.value;
+    });
+    this.start.addEventListener('click', this.startTimer.bind(this));
+    this.pause.addEventListener('click', this.pauseTimer.bind(this));
+    this.stop.addEventListener('click', this.stopTimer.bind(this));
+  },
 
-const checkButtonState = function(button) {
-  if (startedState) {
-    button.disabled = false;
+  startTimer() {
+    // this.started = setInterval(this.tick.bind(this),  20);
+    this.started = requestAnimationFrame(this.tick.bind(this));
+    this.lockBtn(this.start);
+    this.unlockBtn(this.pause);
+    this.unlockBtn(this.stop);
+  },
+
+  pauseTimer() {
+    const {started} = this;
+    // clearInterval(started);
+    cancelAnimationFrame(started);
+    this.unlockBtn(this.start);
+    this.lockBtn(this.pause);
+  },
+
+  stopTimer() { //arrow?
+    const {started} = this;
+    // clearInterval(started);
+    cancelAnimationFrame(started);
+    this.setTimeRemaining(this.startValue);
+    this.unlockBtn(this.start);
+    this.lockBtn(this.pause);
+    this.lockBtn(this.stop);
+  },
+
+  tick() {
+    this.started = requestAnimationFrame(this.tick.bind(this));
+    console.log('tick');
+    if (this.getTimeRemaining() < 0) {
+      this.stopTimer();
+    }
+    this.setTimeRemaining( (this.getTimeRemaining() - 0.02).toFixed(2) );
+  },
+
+  getTimeRemaining() {
+    return +this.input.value;
+  },
+
+  setTimeRemaining(value) {
+    this.input.value = value;
+  },
+
+  lockBtn(btn) {
+    btn.setAttribute('disabled', true);
+  },
+
+  unlockBtn(btn) {
+    btn.removeAttribute('disabled');
+  },
+
+  initialize() {
+    this.eventsListeners();
   }
 };
 
-const getInputValue = function() {
-  return +input.value;
-};
-
-const setInputValue = function(value) {
-  input.value = value;
-};
-
-const timer = function() {
-  const value = getInputValue();
-  setInputValue(tick(value, 0.02).toFixed(2));
-};
-
-const startTimer = function() {
-  startValue = getInputValue();
-  startedState = setInterval(timer, 20);
-  checkButtonState(pause);
-};
-
-const pauseTimer = function() {
-  if (startedState) {
-    clearInterval(startedState);
-    checkButtonState(stop);
-  }
-};
-
-const stopTimer = function() {
-  if (startedState) {
-    clearInterval(startedState);
-    setInputValue(startValue);
-  }
-};
-
-
-
-start.addEventListener('click', startTimer);
-pause.addEventListener('click', pauseTimer);
-stop.addEventListener('click', stopTimer);
-
+timer.initialize();

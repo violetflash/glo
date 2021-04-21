fe4c567188cf23f73d14a93e9f5a3a43939372ca
Alex Grobovsky <ktotaam@mail.ru>
2021-04-21 10:14:49 +0500

    add eslint congif

--- codepen#1/app.js
+++ codepen#1/app.js
@@ -1,33 +1,54 @@
-'use strict';
-
-const calculator = {
-  a: document.querySelector('#a'),
-  b: document.querySelector('#b'),
-  sumBtn: document.querySelector('#sum'),
-  multBtn: document.querySelector('#mult'),
-  res: document.querySelector('#res'),
-
-  sum: function(){
-    const {a, b} = this;
-    this.show(Number(a.value) + Number(b.value));
-  },
-  mult: function(){
-    const {a, b} = this;
-    this.show(a.value * b.value);
-  },
-  show: function(result){
-    this.res.value = result;
-  },
-
-  initialize() {
-    [this.a, this.b].forEach((input) => {
-      input.addEventListener('input', () => {
-        input.value = input.value.replace(/[^\d]/g, '');
-      });
-    });
-    this.sumBtn.addEventListener('click', this.sum.bind(this));
-    this.multBtn.addEventListener('click', this.mult.bind(this));
-  }
+const print1 = function() {
+    console.log('Крот');
+};
+const print2 = function() {
+    console.log('овце,');
+};
+const print3 = function() {
+    console.log('жирафу,');
+};
+const print4 = function() {
+    console.log('зайке');
+};
+const print5 = function() {
+    console.log('голубые');
+};
+const print6 = function() {
+    console.log('сшил');
+};
+const print7 = function() {
+    console.log('фуфайки');
 };
 
-calculator.initialize();
+const func1 = function() {
+    print1();
+};
+
+const func2 = function() {
+    func1();
+
+    setTimeout(() => {
+        // ...
+    }, 1000);
+};
+
+const func3 = function() {
+    setTimeout(() => {
+        func2();
+        // ...
+    }, 250);
+
+    // ...
+};
+
+setTimeout(() => {
+    // ...
+
+    setTimeout(() => {
+        // ...
+    }, 750);
+}, 500);
+
+// ...
+
+func3();

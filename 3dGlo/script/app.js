08efcb53831ac34a380c88f1b5069afedf47ec35
Alex Grobovsky <ktotaam@mail.ru>
2021-04-21 20:53:48 +0500

    fix: remove recursive setInterval call

--- 3dGlo/script/app.js
+++ 3dGlo/script/app.js
@@ -1,42 +1,42 @@
 window.addEventListener('DOMContentLoaded', () => {
-    'use strict';
+  'use strict';
 
-    //Timer
-    function countTimer(deadline) {
-        const timerHours = document.querySelector('#timer-hours'),
-            timerMinutes = document.querySelector('#timer-minutes'),
-            timerSeconds = document.querySelector('#timer-seconds');
+  //Timer
+  function countTimer(deadline) {
+    const timerHours = document.querySelector('#timer-hours'),
+      timerMinutes = document.querySelector('#timer-minutes'),
+      timerSeconds = document.querySelector('#timer-seconds');
 
-        function getTimeRemaining() {
-            const dateStop = new Date(deadline).getTime(),
-                dateNow = new Date().getTime(),
-                timeRemaining = (dateStop - dateNow) / 1000,
-                seconds = timeRemaining > 0 ? Math.floor(timeRemaining % 60) : 0,
-                minutes = timeRemaining > 0 ?Math.floor(timeRemaining / 60 % 60) : 0,
-                hours = timeRemaining > 0 ? Math.floor(timeRemaining / 3600 % 24) : 0;
-            return { timeRemaining, hours, minutes, seconds };
-        }
 
-        function checkZero(num) {
-            return num < 10 ? '0' + num : num;
-        }
-
-        function updateClock() {
-            const timer = getTimeRemaining();
-
-            timerHours.textContent = checkZero(timer.hours);
-            timerMinutes.textContent = checkZero(timer.minutes);
-            timerSeconds.textContent = checkZero(timer.seconds);
-
-            const timerInterval = setInterval(updateClock, 1000);
-
-            if (timer.timeRemaining < 0) {
-                clearInterval(timerInterval);
-            }
-        }
-
-        updateClock();
+    function getTimeRemaining() {
+      const dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = timeRemaining > 0 ? Math.floor(timeRemaining % 60) : 0,
+        minutes = timeRemaining > 0 ? Math.floor(timeRemaining / 60 % 60) : 0,
+        hours = timeRemaining > 0 ? Math.floor(timeRemaining / 3600 % 24) : 0;
+      return {timeRemaining, hours, minutes, seconds};
     }
 
-    countTimer('1 april 2021');
+    function checkZero(num) {
+      return num < 10 ? '0' + num : num;
+    }
+
+    function updateClock() {
+      const timer = getTimeRemaining();
+
+      timerHours.textContent = checkZero(timer.hours);
+      timerMinutes.textContent = checkZero(timer.minutes);
+      timerSeconds.textContent = checkZero(timer.seconds);
+
+      if (timer.timeRemaining < 0) {
+        clearInterval(timerInterval);
+      }
+    }
+
+    updateClock();
+    const timerInterval = setInterval(updateClock, 1000);
+  }
+
+  countTimer('22 april 2021 00:10');
 });

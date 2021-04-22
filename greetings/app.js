4789ee46032d070807423298d4ed83a6a738af7f
Alex Grobovsky <ktotaam@mail.ru>
2021-04-22 08:49:41 +0500

    minor fix

--- greetings/app.js
+++ greetings/app.js
@@ -9,55 +9,55 @@
 'use strict';
 
 const getDate = function() {
-  const today = new Date(),
-    hours = today.getHours(),
-    minutes = today.getMinutes(),
-    seconds = today.getSeconds(),
-    dayNum = today.getDay(),
-    ending = today.toLocaleTimeString('en').split(' ')[1],
-    timeRemaining = '';
+    const today = new Date(),
+        hours = today.getHours(),
+        minutes = today.getMinutes(),
+        seconds = today.getSeconds(),
+        dayNum = today.getDay(),
+        ending = today.toLocaleTimeString('en').split(' ')[1],
+        timeRemaining = '';
 
-  let time = '';
+    let time = '';
 
-  const timeOfDay = hours < 12 ? 'утро!' :
-    (hours > 12 && hours < 17) ? 'день!' :
-      (hours > 17 && hours < 22) ? 'вечер!' :
-        'ночи!';
+    const timeOfDay = hours < 12 ? 'утро!' :
+        (hours > 12 && hours < 17) ? 'день!' :
+            (hours > 17 && hours < 22) ? 'вечер!' :
+                'ночи!';
 
-  const dayName = dayNum === 0 ? 'Воскресенье' :
-    dayNum === 1 ? 'Понедельник' :
-      dayNum === 2 ? 'Вторник' :
-        dayNum === 3 ? 'Среда' :
-          dayNum === 4 ? 'Четверг' :
-            dayNum === 5 ? 'Пятница' :
-              'Суббота';
+    const dayName = dayNum === 0 ? 'Воскресенье' :
+        dayNum === 1 ? 'Понедельник' :
+            dayNum === 2 ? 'Вторник' :
+                dayNum === 3 ? 'Среда' :
+                    dayNum === 4 ? 'Четверг' :
+                        dayNum === 5 ? 'Пятница' :
+                            'Суббота';
 
-  function checkZero(num) {
-    return num < 10 ? '0' + num : num;
-  }
+    function checkZero(num) {
+        return num < 10 ? '0' + num : num;
+    }
 
-  time = `${checkZero(hours)}:${checkZero(minutes)}:${checkZero(seconds)} ${ending}`;
+    time = `${checkZero(hours)}:${checkZero(minutes)}:${checkZero(seconds)} ${ending}`;
 
-  return {time, timeOfDay, dayName};
+    return { time, timeOfDay, dayName };
 };
 
 function getTimeRemaining(deadline) {
     const dateStop = new Date(deadline).getTime(),
-      dateNow = new Date().getTime(),
-      timeRemaining = (dateStop - dateNow) / 1000;
+        dateNow = new Date().getTime(),
+        timeRemaining = (dateStop - dateNow) / 1000;
     return timeRemaining > 0 ? Math.floor(timeRemaining / 3600 / 24) : 0;
 }
 
 const today = getDate();
 
 const greeting = today.timeOfDay === 'Утро' ? 'Доброе' :
-  today.timeOfDay === 'Ночи' ? 'Доброй' :
-    'Добрый';
+    today.timeOfDay === 'Ночи' ? 'Доброй' :
+        'Добрый';
 
 document.querySelector('#greet').innerText = `${greeting} ${today.timeOfDay}`;
 document.querySelector('#day').innerText = `Сегодня: ${today.dayName}`;
 document.querySelector('#time').innerText = `Текущее время: ${today.time}`;
-setInterval(function() {
+setInterval(() => {
     const today = getDate();
     document.querySelector('#time').innerText = `Текущее время: ${today.time}`;
 }, 1000);

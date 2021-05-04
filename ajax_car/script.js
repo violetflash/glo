daaeb51714f63e32f277d663ae82a95df1360577
Alex Grobovsky <ktotaam@mail.ru>
2021-05-04 16:18:40 +0500

    hard 28 with promises

--- ajax_car/script.js
+++ ajax_car/script.js
@@ -3,41 +3,44 @@ document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
     const select = document.getElementById('cars'),
-        output = document.getElementById('output');
+        output = document.getElementById('output'),
+        data = './cars.json';
 
-    const getCar = () => {
+    const getCar = (url) => {
+        return new Promise((resolve, reject) => {
+            const request = new XMLHttpRequest();
+            request.open('GET', url);
+            request.setRequestHeader('Content-type', 'application/json');
+            request.send();
+            request.addEventListener('readystatechange', () => {
 
-        const request = new XMLHttpRequest();
-        request.open('GET', './cars.json');
-        request.setRequestHeader('Content-type', 'application/json');
-        request.send();
-        request.addEventListener('readystatechange', () => {
-            const willGetCar = new Promise(((resolve, reject) => {
+                if (request.readyState !== 4) return;
+
                 if (request.readyState === 4 && request.status === 200) {
                     resolve(request);
                 } else {
-                    reject();
+                    reject(request.status);
                 }
-            }));
-            willGetCar
-                .then(response => {
-                    const data = JSON.parse(response.responseText);
-                    data.cars.forEach(item => {
-                        if (item.brand === select.value) {
-                            const {brand, model, price} = item;
-                            output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-                        }
-                    });
-                })
-                .catch(err => {
-                    console.error(err);
-                    output.innerHTML = 'Произошла ошибка';
-                });
+            });
         });
     };
 
-    select.addEventListener('change', getCar);
-
+    select.addEventListener('change', () => {
+        getCar(data)
+            .then(response => {
+                const data = JSON.parse(response.responseText);
+                data.cars.forEach(item => {
+                    if (item.brand === select.value) {
+                        const {brand, model, price} = item;
+                        output.innerHTML = `Тачка ${brand} ${model} <br>
+                                Цена: ${price}$`;
+                    }
+                });
+            })
+            .catch(err => {
+                console.error(err);
+                output.innerHTML = 'Произошла ошибка';
+            });
+    });
 });
 
